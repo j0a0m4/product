4f178543fa70a469c9534569690df92c4ec8b143
João Lopes <joaomarcoslopes@id.uff.br>
2022-10-16 18:33:14 -0300

    chore: initial commit

---
+++ src/test/java/br/uff/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uff.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
